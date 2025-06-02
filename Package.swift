09e1e80c650083a2dfbb65e158dccb0dda1d30cf
Nyein <pauksohne2t@gmail.com>
2025-06-03 01:53:57 +0200

    Fix tools version to 5.9 for GitHub Actions CI

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // TO:
 @_spi(Private) import SwiftUI
-// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
